26fcd997e4b520c4ca31e637e175b2075d4dacb9
DezEbuwa <dezebuwa@gmail.com>
2025-08-08 11:31:16 -0400

    Initial commit: Add Dez Graph Editor with Node.js server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the current directory
+app.use(express.static('.'));
+
+// Default route serves index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`📁 Serving files from: ${__dirname}`);
+  console.log(`🛑 Press Ctrl+C to stop the server`);
+});
