fdd89f7e48e145f095a1f42decace0f8b75a4fee
DezEbuwa <dezebuwa@gmail.com>
2025-08-08 22:16:17 -0400

    back to normal

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+// Serve static files from the current directory
 app.use(express.static('.'));
 
-
+// Default route serves index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Serving files from: ${__dirname}`);
+  console.log(`Press Ctrl+C to stop the server`);
 });
